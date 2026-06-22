b43df7cfea8ee421c845809cf6882c20b3fe02ad
gupta-heli <gupta-heli@users.noreply.github.com>
2026-06-22 16:00:00 +0530

    feat: read potentiometer to control blink speed (v1.2)

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,15 +1,22 @@
 int blinkCount = 0;
+const int potPin = A0;
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
+  int potValue = analogRead(potPin);
+  // Potentiometer value is 0-1023, map it to a reasonable delay range (100ms to 2000ms)
+  int delayTime = map(potValue, 0, 1023, 100, 2000);
+  
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(delayTime);
   digitalWrite(13, LOW);
-  delay(500);
+  delay(delayTime);
   blinkCount++;
   Serial.print("Blink count: ");
-  Serial.println(blinkCount);
+  Serial.print(blinkCount);
+  Serial.print(" | Delay: ");
+  Serial.println(delayTime);
 }
